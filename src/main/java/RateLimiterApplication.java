5eb06cdc50145339c59d5b59fa546c646ac1901b
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-07-19 21:45:03 +0200

    implement Rate Limiter Application

--- src/main/java/RateLimiterApplication.java
+++ src/main/java/RateLimiterApplication.java
@@ -1,7 +1,14 @@
 public class RateLimiterApplication {
 
     public static void main(String args[]){
-
+        RateLimiter limiter = new RateLimiter(3, 60); // 3 requests per 60 seconds
+        System.out.println(limiter.allowRequest("user1", 1)); // true
+        System.out.println(limiter.allowRequest("user1", 2));  // true
+        System.out.println(limiter.allowRequest("user2", 9));  // true
+        System.out.println(limiter.allowRequest("user1", 3));  // true
+        System.out.println(limiter.allowRequest("user1", 4));  // false
+        System.out.println(limiter.allowRequest("user1", 5));  // false
+        System.out.println(limiter.allowRequest("user1", 61)); // true (window resets)
 
     }
 
